0b19eddb0714f7801b9d673143dafa550f9096fd
Katie <katiewong12780@gmail.com>
2023-07-14 07:18:44 -0700

    Initial Commit

---
+++ GitHub example/GitHub_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_exampleApp.swift
+//  GitHub example
+//
+//  Created by Katie Wong on 2023-07-14.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
